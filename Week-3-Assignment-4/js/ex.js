53ea1fd2bb9f06c3109b17027309a8954f237332
Jasp0005 <jasp0005@algonquinlive.com>
2015-10-05 18:27:12 -0400

    Update js

--- Week-3-Assignment-4/js/ex.js
+++ Week-3-Assignment-4/js/ex.js
@@ -1,3 +1,18 @@
-// JavaScript Document
+var leftBox = document.querySelector(".long-rect");
+var rightBox = document.querySelector(".short-rect");
+var i = false;
 
-
+document.addEventListener("DOMContentLoaded", function() {
+  leftBox.addEventListener("mouseover", function() {
+    rightBox.innerHTML = ("The mouse is over the left box.");
+  });
+  leftBox.addEventListener("mouseout", function() {
+    rightBox.innerHTML = ("The mouse is not over the left box");
+    i = true;
+  });
+  rightBox.addEventListener("click", function() {
+    if (i) {
+      alert("The right box has been clicked.");
+    }
+  });
+});
